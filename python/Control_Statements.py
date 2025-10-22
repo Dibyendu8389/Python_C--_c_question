f6093e7a0c5c1fa9419ad2a8d290905c721e4d05
dibyendu <dibyendup282@gmail.com>
2025-10-22 20:36:32 +0530

    Create Control_Statements.py
    add

---
+++ python/Control_Statements.py
@@ -0,0 +1,61 @@
+#Question: What are control statements in Python?
+#control statement in python means flow or structure of the data control step by differnet comend .
+'''
+if True:
+    print("This runs")
+'''
+
+
+#Question: Write a program to check if a number is positive or negative.
+'''
+x = (int(input("Enter a number:" )))
+if x > 0:
+    print(True)
+else :
+    print(False)
+'''
+
+
+#Question: Write a program to print numbers from 1 to 5 using a "for" loop.
+'''
+for i in range (1,6):
+    print (i)
+'''
+
+
+#Question: Write a program to find the largest of three numbers using if-elif-else.
+'''
+a = (input("Enter a number for first:" ))
+b = (input("Enter a number for second:" ))
+c = (input("Enter a number for theird:" ))
+if a>b and a>c:
+    print("a is largest number",a)
+elif b>c:
+    print("b is largest number",b)
+else:
+    print("c is largest number",c)
+
+'''
+
+
+#Question: Write a program to print even numbers between 1 and 10 using a while loop.
+'''
+i=1
+while i<=10:
+    if i % 2 ==0:
+        print(i)
+    i+=1
+
+'''
+
+
+#Question: Write a program to check whether a given year is a leap year or not.
+
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap year")
+else:
+    print("Not a leap year")
+
+
+
